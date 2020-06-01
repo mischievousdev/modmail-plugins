52170c1456b9a302da544df53ff7424d5390f070
mischievousdev <60783846+mischievousdev@users.noreply.github.com>
2020-06-01 11:59:49 +0000

    Update antiinvite.py

--- antiinvite/antiinvite.py
+++ antiinvite/antiinvite.py
@@ -7,17 +7,17 @@ class Antiinvite(commands.Cog):
         self.bot = bot
 
     @commands.Cog.listener()
-    async def on_message(self, message)
-    match = re.search(r"(?:https?://)?discord(?:(?:app)?\.com/invite|\.gg)/?[a-zA-Z0-9]+/?", message.content)
-    if match:
-        member = message.guild.get_member(message.author.id) # doing this coz message.author is discord.User, but for checking permissions i need discord.Member
-        if member.guild_permissions.manage_server:
-            return
+    async def on_message(self, message):
+        match = re.search(r"(?:https?://)?discord(?:(?:app)?\.com/invite|\.gg)/?[a-zA-Z0-9]+/?", message.content)
+        if match:
+            member = message.guild.get_member(message.author.id) # doing this coz message.author is discord.User, but for checking permissions i need discord.Member
+            if member.guild_permissions.manage_server:
+                return
+            else:
+                await message.delete()
+                await message.channel.send(f"Deleted a invite link posted by {message.author.mention}")
         else:
-            await message.delete()
-            await message.channel.send(f"Deleted a invite link posted by {message.author.mention}")
-    else:
-        return
+            return
 
 def setup(bot):
     bot.add_cog(Antiinvite(bot))
