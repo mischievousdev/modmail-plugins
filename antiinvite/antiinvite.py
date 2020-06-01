e67217bb4edf5e1cf0b0332f6dcab50e00878897
mischievousdev <60783846+mischievousdev@users.noreply.github.com>
2020-06-01 11:56:12 +0000

    Add files via upload

---
+++ antiinvite/antiinvite.py
@@ -0,0 +1,23 @@
+import re
+import discord
+from discord.ext import commands
+
+class Antiinvite(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_message(self, message)
+    match = re.search(r"(?:https?://)?discord(?:(?:app)?\.com/invite|\.gg)/?[a-zA-Z0-9]+/?", message.content)
+    if match:
+        member = message.guild.get_member(message.author.id) # doing this coz message.author is discord.User, but for checking permissions i need discord.Member
+        if member.guild_permissions.manage_server:
+            return
+        else:
+            await message.delete()
+            await message.channel.send(f"Deleted a invite link posted by {message.author.mention}")
+    else:
+        return
+
+def setup(bot):
+    bot.add_cog(Antiinvite(bot))
